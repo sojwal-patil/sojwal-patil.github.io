215cd1e038512883422b3882227b110e0d3527ff
= <sojwalpatil2006@gmail.com>
2026-07-18 02:13:56 +0530

    User Details, Skills and Github Repos Updated

--- static/script.js
+++ static/script.js
@@ -17,3 +17,54 @@ hamburger.addEventListener('click', () => {
 
     hamburger_active = !hamburger_active;
 });
+
+const username = "sojwal-patil";
+const container = document.getElementById("github-projects");
+
+async function loadRepos() {
+    try {
+        const response = await fetch(
+            `https://api.github.com/users/${username}/repos?sort=updated&per_page=6`
+        );
+
+        const repos = await response.json();
+
+        container.innerHTML = "";
+
+        repos
+            .filter(repo => !repo.fork)
+            .forEach(repo => {
+
+                const description = repo.description
+                    ? repo.description.length > 100
+                        ? repo.description.substring(0, 100) + "..."
+                        : repo.description
+                    : "No description";
+
+                container.innerHTML += `
+                    <div class="project-card">
+
+                        <h3>${repo.name}</h3>
+
+                        <p>${description}</p>
+
+                        <div class="project-info">
+                            <span>${repo.language || "Unknown"}</span>
+                            
+                        </div>
+
+                        <a href="${repo.html_url}" target="_blank">
+                            View Repository <i class="fa-solid fa-arrow-up-right-from-square" style="font-size:small;"></i>
+                        </a>
+
+                    </div>
+                `;
+            });
+
+    } catch (error) {
+        container.innerHTML = "<p>Unable to load repositories.</p>";
+        console.error(error);
+    }
+}
+
+loadRepos();
